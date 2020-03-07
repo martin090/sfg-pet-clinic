85ba3731714c30d9bdc26f8b9ea4b35f7cd0928e
Martin Sanguin <sanguinmartin@gmail.com>
2020-03-07 16:19:24 -0300

    adding POJOs.

---
+++ src/main/java/com/martinsanguin/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.martinsanguin.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/martinsanguin/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.martinsanguin.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
