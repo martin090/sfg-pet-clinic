6a4050a2c2782b406383e10b9ba2c99ba5179850
Martin Sanguin <sanguinmartin@gmail.com>
2020-03-21 20:43:40 -0300

    Added pets and contact info for owners on Startup. closes #29

--- pet-clinic-web/src/main/java/com/martinsanguin/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/martinsanguin/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.martinsanguin.sfgpetclinic.bootstrap;
 
 import com.martinsanguin.sfgpetclinic.model.Owner;
+import com.martinsanguin.sfgpetclinic.model.Pet;
 import com.martinsanguin.sfgpetclinic.model.PetType;
 import com.martinsanguin.sfgpetclinic.model.Vet;
 import com.martinsanguin.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.martinsanguin.sfgpetclinic.services.PetTypeService;
 import com.martinsanguin.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -29,15 +32,29 @@ public class DataLoader implements CommandLineRunner {
         dog.setName("dog");
         PetType savedDogPetType = petTypeService.save(dog);
 
+        System.out.println("dot pet type id: " + dog.getId());
+
         PetType cat = new PetType();
-        dog.setName("cat");
+        cat.setName("cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
-        Owner owner = new Owner();
-        owner.setFirstName("Martin");
-        owner.setLastName("Sanguin");
+        System.out.println("cat pet type id: " + cat.getId());
 
-        ownerService.save(owner);
+        Owner martin = new Owner();
+        martin.setFirstName("Martin");
+        martin.setLastName("Sanguin");
+        martin.setAddress("Test 123");
+        martin.setCity("Buenos Aires");
+        martin.setTelephone("1231232");
+
+        Pet martinsPet = new Pet();
+        martinsPet.setPetType(savedDogPetType);
+        martinsPet.setOwner(martin);
+        martinsPet.setName("Amelie");
+        martinsPet.setBirthday(LocalDate.now());
+        martin.getPets().add(martinsPet);
+
+        ownerService.save(martin);
 
         System.out.println("Owners loaded..");
 
