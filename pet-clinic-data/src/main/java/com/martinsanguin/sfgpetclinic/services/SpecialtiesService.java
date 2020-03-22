f255d6489bfd98f17498fba55b5df14fcd6aeaae
Martin Sanguin <sanguinmartin@gmail.com>
2020-03-22 01:41:32 -0300

    Specialties map service implemented. closes #27

---
+++ pet-clinic-data/src/main/java/com/martinsanguin/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.martinsanguin.sfgpetclinic.services;
+
+import com.martinsanguin.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
