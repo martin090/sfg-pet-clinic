0f206e22a336cb6191995339c6ae529bbb84eaa1
Martin Sanguin <sanguinmartin@gmail.com>
2020-03-25 09:59:49 -0300

    Owner entity mapped. closes #32

--- pet-clinic-data/src/main/java/com/martinsanguin/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/martinsanguin/sfgpetclinic/model/Owner.java
@@ -1,12 +1,22 @@
 package com.martinsanguin.sfgpetclinic.model;
 
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
+
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<Pet>();
 
     public String getAddress() {
