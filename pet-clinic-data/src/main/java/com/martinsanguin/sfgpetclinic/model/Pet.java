3bdf24032e421e8b1e4ea477e0bdb1169176f63c
Martin Sanguin <sanguinmartin@gmail.com>
2020-03-25 14:29:41 -0300

    Visit entity mapped. closes #34

--- pet-clinic-data/src/main/java/com/martinsanguin/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/martinsanguin/sfgpetclinic/model/Pet.java
@@ -2,6 +2,9 @@ package com.martinsanguin.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -17,6 +20,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birthday")
     private LocalDate birthday;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -47,5 +53,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
