17e6bb6b4dcb5299388ed112480a7d84ad488ad5
Martin Sanguin <sanguinmartin@gmail.com>
2020-03-26 08:15:33 -0300

    Spring Data JPA Repository for Owners done. closes #36

--- pet-clinic-data/src/main/java/com/martinsanguin/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/martinsanguin/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,5 +3,8 @@ package com.martinsanguin.sfgpetclinic.repositories;
 import com.martinsanguin.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findOwnerByLastName(String lastname);
 }
